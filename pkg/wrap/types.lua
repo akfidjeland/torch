2f9551d8acce8272498515b160737ff2ca77c8fa
Julien Cornebise <julien.cornebise@gmail.com>
2013-05-17 18:15:31 +0100

    Pass address of returned arguments

--- pkg/wrap/types.lua
+++ pkg/wrap/types.lua
@@ -359,7 +359,11 @@ for _,typename in ipairs({"real", "unsigned char", "char", "short", "int", "long
              end,
       
       carg = function(arg)
-                return string.format('arg%d', arg.i)
+                if arg.returned then
+                    return string.format('&arg%d', arg.i)
+                else
+                    return string.format('arg%d', arg.i)
+                end
              end,
 
       creturn = function(arg)
